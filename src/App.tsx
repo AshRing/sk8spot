58d3648645ba923b699c233fcd0b09692c4f05a0
Ash <ashleeringler@gmail.com>
2020-02-25 19:43:55 -0700

    updated feature markers and popup WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 // import InteractiveMap, { Marker } from "react-map-gl";
 import ReactMapboxGl, { Layer, Feature, Popup } from "react-mapbox-gl";
-// import skateboarder from "./_assets/skateboarderSvg.svg";
+import skateboarder from "./_assets/skateboarder.png";
 
 const mapBoxToken =
     "pk.eyJ1IjoiYXNocm5nbHIiLCJhIjoiY2s2Y3J3dmxrMWZmcDNwbnp1dW8zb2t5cCJ9.4LcbsFrSfqhQHIshTewEuw";
@@ -10,27 +10,28 @@ const Map = ReactMapboxGl({
 });
 
 const App = () => {
-    const [markers, setMarkers] = React.useState<number[][]>([
-        [-104.95468543330873, 39.768934798582336],
-    ]);
+    const [markers, setMarkers] = React.useState([{id: "abc123", coords:
+        [-104.95468543330873, 39.768934798582336]}]
+    );
     const [currentMarker, setCurrentMarker] = React.useState<[number, number]>(undefined);
     const [currentPopupMarker, setCurrentPopupMarker] = React.useState<[number, number]>(undefined);
     const [center, setCenter] = React.useState<number[] | any>([
         -104.95468543330873,
         39.768934798582336,
     ]);
-    const [shownFeatures, setShownFeatures] = React.useState(markers);
+    const [shownFeatures, setShownFeatures] = React.useState(markers.map((marker) => marker.coords));
     const mapRef = React.useRef(undefined);
 
     React.useEffect(() => {
-        const showPosition = (_) => {
-            setCenter([-104.98062666065698, 39.82656114282784]);
+        const showPosition = ({coords}) => {
+            setCenter([coords.longitude, coords.latitude]);
         };
         navigator.geolocation.getCurrentPosition(showPosition);
     }, []);
 
     React.useEffect(() => {
-        setShownFeatures(currentMarker ? markers.concat([currentMarker]) : markers);
+        const markerCoords = markers.map((marker) => marker.coords);
+        setShownFeatures(currentMarker ? markerCoords.concat([currentMarker]) : markerCoords);
     }, [currentMarker]);
 
     const handleClick = (e) => {
@@ -45,7 +46,7 @@ const App = () => {
     };
 
     const setMarker = () => {
-        setMarkers([...markers, currentMarker]);
+        setMarkers([...markers, {id: Math.random().toString(), coords: currentMarker}]);
         setCurrentMarker(undefined);
     };
 
@@ -57,15 +58,18 @@ const App = () => {
     const onFeatureClick = (e) => {
         const lngLat: [number, number] = [e.lngLat.lng, e.lngLat.lat];
         setCurrentPopupMarker(lngLat);
-        console.log(markers.find((marker) => marker === lngLat));
+        console.log(markers.map((marker) => marker.coords).find((marker) => marker === lngLat));
     };
+    const image = new Image(30, 30);
+    image.src = skateboarder;
+    const images = ["skateboarder", image];
 
     return (
         <div>
             <h1>MapBox</h1>
             <Map
                 ref={mapRef}
-                style="mapbox://styles/ashrnglr/ck6mh1dcx153x1il7231o83rx"
+                style="mapbox://styles/ashrnglr/ck6sfee7u5weq1inlioqfwnaz"
                 onClick={handleClick}
                 center={center}
                 zoom={[17]}
@@ -79,7 +83,7 @@ const App = () => {
                     cursor: "default",
                 }}
             >
-                <Layer type="circle" id="marker">
+                <Layer type="symbol" id="marker" layout={{ "icon-image": "skateboarder", "icon-allow-overlap": true }} images={images}>
                     {shownFeatures.map((feature, i) => {
                         return (
                             <Feature
@@ -102,7 +106,7 @@ const App = () => {
             <div style={{ position: "absolute", bottom: "50px", left: "20px" }}>
                 {!currentMarker && <button onClick={placeMarker}>Add Marker</button>}
                 {currentMarker && (
-                    <button onClick={setMarker} style={{ marginLeft: "20px" }}>
+                    <button onClick={setMarker}>
                         Set Marker
                     </button>
                 )}
